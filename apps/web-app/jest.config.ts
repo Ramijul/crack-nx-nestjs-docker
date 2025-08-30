a06ff7f06d4eeeb8de95e64221dd7e0e11766c90
rami <ramizul127@gmail.com>
2025-08-30 19:04:19 -0400

    add react application

---
+++ apps/web-app/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: '@crack-nx-nestjs-docker/web-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: 'test-output/jest/coverage',
+};
